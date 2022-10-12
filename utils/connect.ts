73e6139856f94b02ae42900e55446db6627500a3
nawodyaishan <nawodyain@gmail.com>
2022-10-12 22:51:41 +0530

    Add try catch db connect

--- utils/connect.ts
+++ utils/connect.ts
@@ -4,11 +4,19 @@ import config from "config";
 function connect() {
     const dbUri = config.get<string>("dbUri");
 
-    return mongoose.connect(dbUri)
-        .then(() => {
+    // return mongoose.connect(dbUri)
+    //     .then(() => {
+    //     console.log("MongoDB Connection Successfully Initiated")
+    // })
+    //     .catch(() => {
+    //         console.log("MongoDB Connection Error")
+    //         process.exit(1)
+    //     })
+    try {
+        await mongoose.connect(dbUri);
         console.log("MongoDB Connection Successfully Initiated")
-    })
-        .catch(() => {
-            console.log("MongoDB Connection Error")
-        })
+    } catch (error) {
+        console.log("MongoDB Connection Error")
+        process.exit(1)
+    }
 }
