d50d620c57aae71f85975a4573d3ec42072bcf2f
nawodyaishan <nawodyain@gmail.com>
2022-11-07 17:13:15 +0530

    Add userModel bcrypt password hash salt

--- src/models/user.Model.ts
+++ src/models/user.Model.ts
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
 import config from "config";
+import {promise} from "zod";
 
 
 export interface UserDocument extends mongoose.Document {
@@ -8,7 +9,9 @@ export interface UserDocument extends mongoose.Document {
     name: string,
     password: string,
     createdAt: Date,
-    updatedAt: Date
+    updatedAt: Date,
+
+    comparePassword(candidatePassword: string): Promise<boolean>
 }
 
 const userSchema = new mongoose.Schema({
@@ -21,6 +24,28 @@ const userSchema = new mongoose.Schema({
 }, {
     timestamps: true,
 });
+
+userSchema.pre("save", async function (next) {
+        let user = this as UserDocument
+
+        if (!(user.isModified("password"))) {
+            return next();
+        }
+
+        // Configure Salt
+        const salt = await bcrypt.genSalt(config.get<number>(`saltWorkFactor`));
+
+        // Hashing Password
+        const hash = await bcrypt.hashSync(user.password, salt);
+        user.password = hash;
+    }
+)
+
+// Comparing User passwords
+userSchema.methods.comparePassword = async function (candidatePassword: string): Promise<boolean> {
+    const user = this as UserDocument
+    return bcrypt.compare(candidatePassword, user.password).catch((e) => false)
+}
 
 // Creating User Model
 const UserModel = mongoose.model("User", userSchema)
